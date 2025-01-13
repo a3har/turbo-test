443b6d488321539b147bd799feab5bc22d14f6a6
Muhammad Azhar <azharmuham13@hotmail.com>
2025-01-13 18:39:56 +0530

    fix nextjs config

--- apps/product-frontend/apps/web/next.config.js
+++ apps/product-frontend/apps/web/next.config.js
@@ -1,13 +1,4 @@
-import { fileURLToPath } from "url"
-import { dirname } from "path"
-import path from "path"
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-  output: "standalone",
-}
+const nextConfig = {}
 
 export default nextConfig
